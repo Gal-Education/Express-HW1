3bced93c629242f32a15849a6ee10cbdb2558549
Gal Amitai <galamitai@Gals-MacBook-Pro.local>
2017-05-07 16:33:39 +0300

    First Commit
    Include API Documentation, files and module

---
+++ function.js
@@ -0,0 +1,73 @@
+const data = require('./data/comingSoonMovies.json');
+
+exports.getAllData = () => {
+	return data;
+}
+
+exports.getMovieById = (id) => {
+	let isFound = false;
+	for(let i in data.movies) {
+		var movieId = data.movies[i];
+		if(movieId.id == id) {
+			console.log(`Movie id number: ${i} has found successfuly.`);
+			isFound = true;
+			return movieId;
+		}
+	}
+	return false;
+}
+
+exports.getMovieDataFilter = (year,category) => {
+	var query = `[`,
+		count = 0;
+
+	let isYear = false,
+		isCat = false;
+
+	for(let i in data.movies) {
+		var _year = data.movies[i];
+	
+		if(_year.year == year) {
+			isYear = true;
+			for(let j in _year.category) {
+				var _cat = _year.category[j];
+				if(_cat == category) {
+					isCat = true;
+				}
+			}
+		}
+		if(isYear && isCat) {
+			console.log(`Movie id number: ${data.movies[i].name} has found.`);
+			query += JSON.stringify(data.movies[i]) + ",";
+			count++;
+		}
+		isYear = false;
+		isCat = false;
+	}
+	if(count == 0) {
+		console.log(`No Results for filter.`);
+		return false;
+	}
+	else {
+		console.log("Total results:" + count);
+		query = query.substr(0,query.length-1);
+		query += `]`;
+		query = JSON.parse(query);
+		return query;
+	}
+}
+
+exports.error = () => {
+	return `<html>
+					<head>
+					<title>404 NOT FOUND</title>
+					</head>
+
+					<body bgcolor="linen" text="black">
+					<div style="font-size: 400px; text-align: center;">? 404 ?</div>
+					<div style="font-size: 128px; text-align: center; font-variant: small-caps;">Not Found</div>
+					<div style="text-align: center;">The document you requested could not be found. Please check the URL and try again. This is a recording.</div>
+					</body>
+					</html>
+					`;
+}
---
+++ index.js
@@ -0,0 +1,50 @@
+const	express = require('express'),
+		app = express(),
+		bodyParser = require('body-parser'),
+		func = require('./function.js'),
+		port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/getAllComingSoon/",
+	(req, res) => {
+		res.json(func.getAllData());
+});
+
+app.post("/getMovieDataById/",
+	(req, res) => {
+		var id = req.body.movieId;
+		var result = func.getMovieById(id);
+		if(result == false) {
+			console.log(`Movie id number: ${id} has not found.`);
+			res.status(200).json({"Error" : "Movie id " + id + " has not found."});
+		}
+		console.log(`Movie id number: ${id} has found.`);
+		res.status(200).json(result);
+});
+
+app.get("/getMovieDataFilter/:year/:category",
+	(req, res) => {
+		var year = req.params.year,
+			cat = req.params.category;
+
+		var result = func.getMovieDataFilter(year,cat);
+		if(result == false) {
+			res.status(200).json({"Error" : "No Result"});
+		}
+		else {
+			res.status(200).send(result);
+		}
+		
+});
+
+app.all('*',
+	(req, res) => {
+		res.send(func.error());
+});
+
+app.listen(port,
+	() => {
+		console.log(`listening to port ${port}`);
+});
